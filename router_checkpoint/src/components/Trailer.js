96197a7f3de4e3062ca4c3f83755ea6b805fdef9
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-07-05 14:23:52 -0400

    Update Trailer.js

--- router_checkpoint/src/components/Trailer.js
+++ router_checkpoint/src/components/Trailer.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
-import {Link} from 'react-router-dom';
-
-
+import React from "react";
+import ReactPlayer from "react-player";
+import { Link } from "react-router-dom";
 
 function Trailer(props) {
- 
-    const trailer= props.location.state
-    return (
-        <div>
-     
-    <h1>Movie's Trailer</h1>
-    <ReactPlayer url={trailer} />
-  
+  const trailer = props.location.state;
+  return (
+    <div>
+      <h1>Movie's Trailer</h1>
+      <ReactPlayer url={trailer} />
 
-    <Link to='/'>previous</Link>
-        </div>
-
-    )
+      <Link to="/">previous</Link>
+    </div>
+  );
 }
 
 export default Trailer;
