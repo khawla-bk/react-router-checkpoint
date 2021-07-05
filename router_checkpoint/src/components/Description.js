fe520d63fef8b4cbce9d3ad6bde8658de4f76590
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-07-05 14:22:24 -0400

    Update Description.js

--- router_checkpoint/src/components/Description.js
+++ router_checkpoint/src/components/Description.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Description(props) {
-  const description = props.location.state;
+  const Description = props.location.state;
 
   return (
     <div>
       <h1> Movie's Description</h1>
-      <p>{description}</p>
+      <h3>{Description}</h3>
 
       <Link to="/">previous</Link>
     </div>
