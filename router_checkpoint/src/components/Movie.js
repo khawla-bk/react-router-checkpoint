592c1c9d23e5c95f30e448c8f2b74083223384cb
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-07-07 10:06:38 -0400

    Create Movie.js

---
+++ router_checkpoint/src/components/Movie.js
@@ -0,0 +1,37 @@
+import { Card } from "react-bootstrap";
+import { useHistory, useParams } from "react-router-dom";
+
+function Movie({ movies, name, trailer, description }) {
+  const history = useHistory();
+  const { ID } = useParams();
+  const Mov = movies.find((movie, i) => movie.id === Number(ID));
+
+  return (
+    <div>
+      <Card>
+        <iframe
+          width="560"
+          height="315"
+          src={Mov.trailer}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+
+        <Card.Body>
+          <Card.Title>{Mov.name}</Card.Title>
+          <Card.Text>{Mov.description}</Card.Text>
+
+          <button
+            className="button"
+            onClick={() => history.goBack()}
+            variant="primary"
+          >
+            Go Back
+          </button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+export default Movie;
